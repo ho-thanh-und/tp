d7cc193505cf08148ab140f4499c7daafbf7d977
HarishB99 <e1138766@u.nus.edu>
2025-03-09 20:39:27 +0800

    RemarkCommand: THrow exception to match expected behaviour

--- src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
+++ src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
@@ -1,13 +1,11 @@
 package seedu.address.logic.commands;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.RemarkCommand.MESSAGE_NOT_IMPLEMENTED_YET;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import seedu.address.logic.parser.AddressBookParser;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
@@ -16,6 +14,6 @@ public class RemarkCommandTest {
     @Test
     public void execute() {
         Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
-        assertCommandFailure(new RemarkCommand(), model, "MESSAGE_NOT_IMPLEMENTED_YET");
+        assertCommandFailure(new RemarkCommand(), model, MESSAGE_NOT_IMPLEMENTED_YET);
     }
 }
