a9980596a8b6f6e172960be4176f09bb82aecc45
enzo <cyenzot@gmail.com>
2025-03-12 19:32:38 +0800

    Update code to better support new field

--- src/main/java/seedu/address/model/person/JobTitle.java
+++ src/main/java/seedu/address/model/person/JobTitle.java
@@ -9,14 +9,14 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
  */
 public class JobTitle {
     public static final String MESSAGE_CONSTRAINTS =
-            "Job title should only contain alphanumeric characters, special characters and spaces, "
+            "Job title should only contain alphanumeric characters, \"/\" \"(\" \")\" and spaces, "
                 + "and it should not be blank";
 
     /*
      * The first character of the address must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
-    public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
+    public static final String VALIDATION_REGEX = "[\\p{Alnum}/()][\\p{Alnum}/() ]*";
 
     public final String value;
 
