eaa6c0a5eaa0cccba4f4bb3d91637e7a24c11be5
HarishB99 <e1138766@u.nus.edu>
2025-03-15 00:09:20 +0800

    fix: Include job title and schedule when adding remark

--- src/main/java/seedu/address/logic/commands/RemarkCommand.java
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -54,7 +54,8 @@ public class RemarkCommand extends Command {
         Person personToEdit = lastShownList.get(index.getZeroBased());
         Person editedPerson = new Person(
                 personToEdit.getName(), personToEdit.getPhone(), personToEdit.getEmail(),
-                personToEdit.getAddress(), remark, personToEdit.getTags());
+                personToEdit.getAddress(), personToEdit.getJobTitle(), personToEdit.getSchedule(),
+                remark, personToEdit.getTags());
 
         model.setPerson(personToEdit, editedPerson);
         model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
