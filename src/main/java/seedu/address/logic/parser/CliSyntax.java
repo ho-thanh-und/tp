ece2c9a52494041251c0956c18475cc325f91994
HarishB99 <e1138766@u.nus.edu>
2025-03-27 21:10:48 +0800

    fix: Update Prefix constructor calls for save command fields

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -17,8 +17,8 @@ public class CliSyntax {
     public static final Prefix PREFIX_REMARK = new Prefix("r/", "Remark");
 
     /* Prefix and suffix definitions - specific to save command */
-    public static final Prefix PREFIX_FILE = new Prefix("p/");
-    public static final Prefix SUFFIX_SAVE_ALL = new Prefix("/a");
-    public static final Prefix SUFFIX_OVERRIDE_FILE = new Prefix("/f");
+    public static final Prefix PREFIX_FILE = new Prefix("p/", "FilePath");
+    public static final Prefix SUFFIX_SAVE_ALL = new Prefix("/a", "");
+    public static final Prefix SUFFIX_OVERRIDE_FILE = new Prefix("/f", "");
 
 }
