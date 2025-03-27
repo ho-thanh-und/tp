62bc636913a37030c08e8109173fd147f4cbe37d
Chi Thanh <e1129981@u.nus.edu>
2025-03-27 16:09:53 +0800

    Fix checkstyle issues

--- src/test/java/seedu/address/logic/commands/ViewStatsCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ViewStatsCommandTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
