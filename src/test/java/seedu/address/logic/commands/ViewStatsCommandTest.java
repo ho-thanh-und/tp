47e5606e9802aa66a110761d7b64e6888fe9cef6
Chi Thanh <e1129981@u.nus.edu>
2025-03-27 15:53:16 +0800

    Add ViewStats test files

---
+++ src/test/java/seedu/address/logic/commands/ViewStatsCommandTest.java
@@ -0,0 +1,56 @@
+package seedu.address.logic.commands;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.CommandResult;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.person.Person;
+import seedu.address.testutil.PersonBuilder;
+
+public class ViewStatsCommandTest {
+
+    @Test
+    public void execute_noPersons_emptyStatsMessage() throws CommandException {
+        Model model = new ModelManager();
+        ViewStatsCommand command = new ViewStatsCommand();
+        CommandResult result = command.execute(model);
+        String expectedMessage = "Statistics:\n";
+        assertEquals(expectedMessage, result.getFeedbackToUser());
+    }
+
+    @Test
+    public void execute_somePersons_showsCorrectStats() throws CommandException {
+        Model model = new ModelManager();
+
+        Person softwareEngineer = new PersonBuilder()
+                .withName("Alice")
+                .withJobTitle("Software Engineer")
+                .build();
+        Person dataScientist = new PersonBuilder()
+                .withName("Bob")
+                .withJobTitle("Data Scientist")
+                .build();
+        Person uiDesigner = new PersonBuilder()
+                .withName("Charlie")
+                .withJobTitle("UI Designer")
+                .build();
+
+        model.addPerson(softwareEngineer);
+        model.addPerson(dataScientist);
+        model.addPerson(uiDesigner);
+
+        ViewStatsCommand command = new ViewStatsCommand();
+        CommandResult result = command.execute(model);
+
+        String expectedMessage = "Statistics:\n"
+                + "Data Scientist: 1\n"
+                + "Software Engineer: 1\n"
+                + "UI Designer: 1\n";
+
+        assertEquals(expectedMessage, result.getFeedbackToUser());
+    }
+}
