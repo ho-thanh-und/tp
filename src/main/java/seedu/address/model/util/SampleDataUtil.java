4700d85a6430fcd1244374778fe2f2ce65cf2dfe
enzo <cyenzot@gmail.com>
2025-04-07 20:27:18 +0800

    Include relevant tags

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -33,23 +33,23 @@ public class SampleDataUtil {
         return new Person[]{
             new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
                 new Address("Blk 30 Geylang Street 29, #06-40"), new Label("Unreviewed"),
-                EMPTY_REMARK, getJobRoleSet("Data Scientist"), getTagSet("friends")),
+                EMPTY_REMARK, getJobRoleSet("Data Scientist"), getTagSet("Java")),
             new Person(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
                 new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"), new Label("Unreviewed"),
-                EMPTY_REMARK, getJobRoleSet("Front End Developer"),
-                getTagSet("colleagues", "friends")),
+                EMPTY_REMARK, getJobRoleSet("Front End Developer"), getTagSet("Java", "Python")),
             new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
                 new Address("Blk 11 Ang Mo Kio Street 74, #11-04"), new Label("Unreviewed"),
-                EMPTY_REMARK, getJobRoleSet("Back End Developer"), getTagSet("neighbours")),
+                EMPTY_REMARK, getJobRoleSet("Back End Developer"), getTagSet("Rust", "Zig")),
             new Person(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
                 new Address("Blk 436 Serangoon Gardens Street 26, #16-43"), new Label("Unreviewed"),
-                EMPTY_REMARK, getJobRoleSet("DevOps Engineer"), getTagSet("family")),
+                EMPTY_REMARK, getJobRoleSet("DevOps Engineer"),
+                getTagSet("React", "MongoDB", "Nodejs", "Expressjs")),
             new Person(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
                 new Address("Blk 47 Tampines Street 20, #17-35"), new Label("Unreviewed"),
-                EMPTY_REMARK, getJobRoleSet("UI Designer"), getTagSet("classmates")),
+                EMPTY_REMARK, getJobRoleSet("UI Designer"), getTagSet("CUDA")),
             new Person(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
                 new Address("Blk 45 Aljunied Street 85, #11-31"), new Label("Unreviewed"),
-                EMPTY_REMARK, getJobRoleSet("QA Engineer"), getTagSet("colleagues"))
+                EMPTY_REMARK, getJobRoleSet("QA Engineer"), getTagSet("C", "Rust", "Go"))
         };
     }
 
